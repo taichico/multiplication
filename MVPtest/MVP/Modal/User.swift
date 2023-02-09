49f7ec5194ca0a41532b80a96e088d93c44cfd06
yoshiokataichi <yoshioka@ars-cube.blue>
2023-02-09 18:53:50 +0900

    first commit

---
+++ MVPtest/MVP/Modal/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  MVPtest
+//
+//  Created by 吉岡泰知 on 2023/02/09.
+//
+
+import Foundation
---
+++ MVPtest/MVP/Presenter/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  MVPtest
+//
+//  Created by 吉岡泰知 on 2023/02/09.
+//
+
+import Foundation
